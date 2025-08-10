f10ac3cb14dafb9166f3e472c9942c93d6534992
bmasciotra-square-digital <brad@squaredigital.com>
2025-08-09 20:24:44 -0700

    adding animation on recipe cards

--- src/components/common/recipe-card-grid/recipe-card-grid.component.ts
+++ src/components/common/recipe-card-grid/recipe-card-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, ViewChild } from '@angular/core';
 import { RecipeCard } from '../../../interfaces/recipeCard';
 
 @Component({
@@ -7,7 +7,30 @@ import { RecipeCard } from '../../../interfaces/recipeCard';
   templateUrl: './recipe-card-grid.component.html',
   styleUrl: './recipe-card-grid.component.scss',
 })
-export class RecipeCardGridComponent {
+export class RecipeCardGridComponent implements AfterViewInit {
+  @ViewChild("cardsContainer") cardsContainer!: ElementRef<HTMLElement>;
+
   public recipes = input<RecipeCard[]>([]);
   public title = input<string>('Featured Recipes');
+
+  ngAfterViewInit() {
+    this.setupScrollAnimation();
+  }
+
+  private setupScrollAnimation() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate-in');
+        }
+      });
+    }, {
+      threshold: 0.1, // Trigger when 10% of the element is visible
+      rootMargin: '0px 0px -50px 0px' // Start animation slightly before element is fully in view
+    });
+
+    if (this.cardsContainer?.nativeElement) {
+      observer.observe(this.cardsContainer.nativeElement);
+    }
+  }
 }
