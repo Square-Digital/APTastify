4176267879ba2c3fae7e13b594af84b52eb8b158
bmasciotra-square-digital <brad@squaredigital.com>
2025-08-24 15:12:12 -0700

    adding new logos

--- src/components/common/separation-bar/separation-bar.component.ts
+++ src/components/common/separation-bar/separation-bar.component.ts
@@ -4,9 +4,16 @@ import { Component, Input, input } from '@angular/core';
   selector: 'aptastify-separation-bar',
   standalone: false,
   templateUrl: './separation-bar.component.html',
-  styleUrl: './separation-bar.component.scss'
+  styleUrl: './separation-bar.component.scss',
 })
 export class SeparationBarComponent {
+  public text = input('');
 
-  public text = input("");
+  public logos = [
+    'assets/images/icons/agriculture.png',
+    'assets/images/icons/hotelier.png',
+    'assets/images/icons/individual.png',
+    'assets/images/icons/trading.png',
+    'assets/images/icons/distrubutor.png',
+  ];
 }
